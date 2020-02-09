70aba3a46c3e2a023756ca7cc5a3d27d9204e77e
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2020-02-09 23:47:11 +0900

    서비스 워커에서 캐시 기능 삭제, 기타 스크립트 오류 수정

--- script.js
+++ script.js
@@ -90,10 +90,3 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker Registered');
   });
 }
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  showInstallPromotion();
-});
--- service-worker.js
+++ service-worker.js
@@ -3,32 +3,6 @@
  */
 
 const _version = 'v1';
-const cacheName = 'v1';
-const cacheList = [
-  '/',
-  '/manifest.json',
-  '/service-worker.js',
-  '/script.js',
-  '/style.css',
-  '/settings/',
-  '/settings/script.js',
-  '/settings/style.css',
-  '/settings/OneSignalSDKWorker.js',
-  '/settings/OneSignalSDKUpdaterWorker.js',
-  '/icons/192',
-  '/icons/512',
-  '/favicon.ico',
-  'https://cdn.jsdelivr.net/npm/bootstrap@4.4.0/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js',
-  'https://cdn.jsdelivr.net/npm/spoqa-han-sans@2.1.2/css/SpoqaHanSans-kr.css',
-  'https://cdn.jsdelivr.net/npm/spoqa-han-sans@2.1.2/Subset/SpoqaHanSans/SpoqaHanSansRegular.woff2',
-  'https://cdn.jsdelivr.net/npm/spoqa-han-sans@2.1.2/Subset/SpoqaHanSans/SpoqaHanSansBold.woff2',
-  'https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js',
-  'https://cdn.jsdelivr.net/npm/sweetalert2@9.5.4/dist/sweetalert2.all.min.js',
-  'https://cdn.jsdelivr.net/npm/swiper@5.3.0/css/swiper.min.css',
-  'https://cdn.jsdelivr.net/npm/swiper@5.3.0/js/swiper.min.js',
-  'https://cdn.onesignal.com/sdks/OneSignalSDK.js'
-];
 
 const log = msg => {
   console.log(`[ServiceWorker ${_version}] ${msg}`);
@@ -38,33 +12,15 @@ const log = msg => {
 self.addEventListener('install', event => {
   self.skipWaiting();
   log('INSTALL');
-  caches.open(cacheName).then(cache => {
-    log('Caching app shell');
-    return cache.addAll(cacheList);
-  })
 });
 
 // Life cycle: ACTIVATE
 self.addEventListener('activate', event => {
   log('Activate');
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== cacheName) {
-          log('Removing old cache ' + key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
 });
 
 // Functional: FETCH
 self.addEventListener('fetch', event => {
   log('Fetch ' + event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
