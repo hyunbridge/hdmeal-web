176492febb4045316b8844d316b727346c93478d
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2020-04-27 23:41:23 +0900

    디자인 수정 및 데이터 불러오기 알고리듬 변경
    헤더(날짜) 텍스트 정렬 변경
    그리드 breakpoint 변경
    데이터 모두 불러올 때 까지 로딩 애니메이션 유지되도록 변경
    데이터를 불러오지 못했을 경우 예외처리

--- script.js
+++ script.js
@@ -13,85 +13,109 @@ new Swiper( '.swiper-container', {
     },
     initialSlide: 1,
 });
-$.getJSON("https://static.api.hdml.kr/data.json", function(data) {
-//$.getJSON("data.json", function(data) {
 
-  function load(name, date, koname, grade, class_) {
+// 데이터 불러오기
+function fetchData() {
+  $.getJSON("https://static.api.hdml.kr/data.json", function (data) {
 
-    $("#"+name+" #date").text(koname+", "+data[date].Date);
+    function load(name, date, koname, grade, class_) {
 
-    menu = data[date].Meal.Menu;
-    $("#"+name+" #menuBody").text("");
-    if (!menu){
-      $("#"+name+" #menuBody").text("식단정보가 없습니다.");
-    } else if (menu[0] == "") {
-      $("#"+name+" #menuBody").text("식단정보가 없습니다.");
-    } else {
-      for (var menuLoc in menu){
-        $("#"+name+" #menuBody").append("<li>"+menu[menuLoc]+"</li>");
-      }
-    }
+      $("#" + name + " #date").text(koname + ", " + data[date].Date);
 
-    $("#"+name+" #ttTitle").text(grade+"학년 "+class_+"반 시간표");
-    if (data[date].Timetable) {
-      tt = data[date].Timetable[grade][class_];
-      console.log(tt);
-      $("#"+name+" #ttBody").text("");
-      if (!tt){
-        $("#"+name+" #ttBody").text("시간표가 없습니다.");
-      } else if (tt.length == 0) {
-        $("#"+name+" #ttBody").text("시간표가 없습니다.");
+      menu = data[date].Meal.Menu;
+      $("#" + name + " #menuBody").text("");
+      if (!menu) {
+        $("#" + name + " #menuBody").text("식단정보가 없습니다.");
+      } else if (menu[0] == "") {
+        $("#" + name + " #menuBody").text("식단정보가 없습니다.");
       } else {
-        for (var ttLoc in tt){
-          $("#"+name+" #ttBody").append("<li>"+tt[ttLoc]+"</li>");
+        for (var menuLoc in menu) {
+          $("#" + name + " #menuBody").append("<li>" + menu[menuLoc] + "</li>");
         }
       }
-    } else {
-      $("#"+name+" #ttBody").text("시간표가 없습니다.");
-    }
 
-    schdl = data[date].Schedule;
-    $("#"+name+" #schdlBody").text("");
-    if (!schdl){
-      $("#"+name+" #schdlBody").text("학사일정이 없습니다.");
-    } else if (schdl[0] == "") {
-      $("#"+name+" #schdlBody").text("학사일정이 없습니다.");
-    } else {
-      for (var schdlLoc in schdl){
-        $("#"+name+" #schdlBody").append("<li>"+schdl[schdlLoc]+"</li>");
+      $("#" + name + " #ttTitle").text(grade + "학년 " + class_ + "반 시간표");
+      if (data[date].Timetable) {
+        tt = data[date].Timetable[grade][class_];
+        console.log(tt);
+        $("#" + name + " #ttBody").text("");
+        if (!tt) {
+          $("#" + name + " #ttBody").text("시간표가 없습니다.");
+        } else if (tt.length == 0) {
+          $("#" + name + " #ttBody").text("시간표가 없습니다.");
+        } else {
+          for (var ttLoc in tt) {
+            $("#" + name + " #ttBody").append("<li>" + tt[ttLoc] + "</li>");
+          }
+        }
+      } else {
+        $("#" + name + " #ttBody").text("시간표가 없습니다.");
+      }
+
+      schdl = data[date].Schedule;
+      $("#" + name + " #schdlBody").text("");
+      if (!schdl) {
+        $("#" + name + " #schdlBody").text("학사일정이 없습니다.");
+      } else if (schdl[0] == "") {
+        $("#" + name + " #schdlBody").text("학사일정이 없습니다.");
+      } else {
+        for (var schdlLoc in schdl) {
+          $("#" + name + " #schdlBody").append("<li>" + schdl[schdlLoc] + "</li>");
+        }
       }
     }
-  }
 
-if (localStorage.Grade && localStorage.Class) {
-  load("yesterday", "Yesterday", "어제", localStorage.Grade, localStorage.Class);
-  load("today", "Today", "오늘", localStorage.Grade, localStorage.Class);
-  load("tomorrow", "Tomorrow", "내일", localStorage.Grade, localStorage.Class);
-} else {
-  load("yesterday", "Yesterday", "어제", 1, 1);
-  load("today", "Today", "오늘", 1, 1);
-  load("tomorrow", "Tomorrow", "내일", 1, 1);
+    if (localStorage.Grade && localStorage.Class) {
+      load("yesterday", "Yesterday", "어제", localStorage.Grade, localStorage.Class);
+      load("today", "Today", "오늘", localStorage.Grade, localStorage.Class);
+      load("tomorrow", "Tomorrow", "내일", localStorage.Grade, localStorage.Class);
+    } else {
+      load("yesterday", "Yesterday", "어제", 1, 1);
+      load("today", "Today", "오늘", 1, 1);
+      load("tomorrow", "Tomorrow", "내일", 1, 1);
+    }
+  }).done(function(){
+    // 로딩 끝나면 화면전환
+    $(".lds-ring").animate(
+      {
+        opacity: "0"
+      },
+      500
+    );
+    $(".lds-ring").remove();
+    $(".swiper-container").animate(
+      {
+        opacity: "1"
+      },
+      500
+    );
+    $(".swiper-button").delay(1000).animate({ opacity: '0' }, 500);
+  }).fail(function(xhr, status, error){
+    console.log(xhr);
+    console.log(status);
+    console.log(error);
+    // 로딩 실패했을 경우
+    Swal.fire({
+      icon: "error",
+      title: "불러오지 못했습니다.",
+      text: "컴퓨터의 인터넷 연결을 확인해 주세요.",
+      confirmButtonText: "다시 시도",
+      customClass: {
+        actions: 'swal-vertical-buttons',
+        confirmButton: 'btn btn-primary btn-lg mb-2'
+      },
+      buttonsStyling: false,
+      heightAuto: false,
+      allowEscapeKey : false,
+      allowOutsideClick: false
+    }).then(result => {
+      if (result.value) {
+        fetchData();
+      }
+    });
+  });
 }
-});
-
-// 로딩 끝나면 화면전환
-$(document).ready(function() {
-  $(".lds-ring").animate(
-    {
-      opacity: "0"
-    },
-    500
-  );
-  $(".lds-ring").remove();
-  $(".swiper-container").animate(
-    {
-      opacity: "1"
-    },
-    500
-  );
-  $(".swiper-button").delay(1000).animate({opacity:'0'},500);
-});
-
+fetchData();
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js').then(() => {
